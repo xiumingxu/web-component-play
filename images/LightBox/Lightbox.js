980cd69d320ea8d2842aebb6cbd04535bf71e1a0
XIX <xiuming.x@outlook.com>
2019-11-28 11:03:59 -0800

    animation js(a lot to do)

---
+++ images/LightBox/Lightbox.js
@@ -0,0 +1,6 @@
+export default class Lightbox {
+	constructor () {
+		alert('test');
+	}
+}
+// module.export = Lightbox;
--- images/LightBox/script.js
+++ images/LightBox/script.js
@@ -1,9 +1,4 @@
-for (let i = 0; i < 10; i++) {
-	let a = document.createElement("a");
-	a.innerHTML = i + "<br>";
-	a.addEventListener("click", function (e) {
-		// e.preventDefault();
-		alert(i);
-	});
-	document.body.appendChild(a);
-}
+import Lightbox from './Lightbox.js';
+// let Lightbox = require('./Lightbox');
+
+let lightbox = new Lightbox();
