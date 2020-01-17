4c6128bdb95b4fa83291ccd5812b09658917e58d
XIX <xiuming.x@outlook.com>
2020-01-17 13:40:26 -0800

    add files

--- CSSTutorial/chapter9/build/07-webpack-main.js
+++ CSSTutorial/chapter9/build/07-webpack-main.js
@@ -80,11 +80,23 @@ document.addEventListener('DOMContentLoaded', function(){
 
 var styles = __webpack_require__(2);
 
-exports.init = function($dom){
-    $dom.innerHTML = '<p class="' + styles.p + '">我是一个自给自足的组件</p>' +
-        '<p class="' + styles.p + ' ' + styles.red + '">我有时候是红的</p>' +
-        '<p class="' + styles.p + ' ' + styles.green + '">有时候是绿的</p>';
-}
+// 用类名 这样可以 组件的样式冲突问题
+exports.init = function ($dom) {
+	$dom.innerHTML =
+		'<p class="' +
+		styles.p +
+		'">我是一个自给自足的组件</p>' +
+		'<p class="' +
+		styles.p +
+		' ' +
+		styles.red +
+		'">我有时候是红的</p>' +
+		'<p class="' +
+		styles.p +
+		' ' +
+		styles.green +
+		'">有时候是绿的</p>';
+};
 
 
 /***/ }),
--- CSSTutorial/chapter9/src/postcss.config.js
+++ CSSTutorial/chapter9/src/postcss.config.js
@@ -1,16 +1,18 @@
-// const autoprefixer = require('autoprefixer');
-// const cssnano = require('cssnano');
-// const atImport = require('postcss-import');
-// const cssnext = require('postcss-cssnext');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const atImport = require('postcss-import');
+const cssnext = require('postcss-cssnext');
 const precss = require('precss');
 module.exports = {
-    plugins: [
-        // atImport,
-        // cssnext,
-        precss
-        /* autoprefixer({
-            browsers:['Firefox > 1']
-        }), */
-        // cssnano
-    ]
+	plugins : [
+		atImport,
+
+		precss,
+		cssnext,
+		autoprefixer({
+			browsers : [ 'Firefox > 1' ]
+		})
+		// cssnano
+	]
+	// output  : '../build/'
 };
