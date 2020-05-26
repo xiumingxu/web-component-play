f5b6a4acd62f1116949a5662f4f5182946b82cb2
XIX <xiuming.x@outlook.com>
2020-05-26 16:03:22 -0700

    babel config

--- Framework/babel-pg/script.js
+++ Framework/babel-pg/script.js
@@ -1,9 +1,16 @@
-for (let i = 0; i < 10; i++) {
-	let a = document.createElement("a");
-	a.innerHTML = i + "<br>";
-	a.addEventListener("click", function (e) {
-		// e.preventDefault();
-		alert(i);
-	});
-	document.body.appendChild(a);
-}
+// import '@babel/polyfill'
+
+const sum = (a, b)=>(a + b);
+
+
+//新的 API
+Promise.resolve(100).then(data=> data);
+
+
+//新的 API
+[10,20,30].includes(20);
+
+
+//只检查语法
+
+//不会处理模块化 ---》 webpack处理模块化
