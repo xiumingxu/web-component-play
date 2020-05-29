88edef5348ab82222bd3230052667d5f69739ff1
XIX <xiuming.x@outlook.com>
2020-05-28 18:05:49 -0700

    get used to {}  and curry

---
+++ JSFeatures/pubSub-2.js
@@ -0,0 +1,55 @@
+const _ = require('lodash');
+
+const PubProto = ()=>{
+    let sublist = {
+    };
+    return {
+        subscribe: function(channel, subscriber){
+            if (!sublist[channel]){
+                sublist[channel] = [];
+            }
+            if (sublist[channel].includes(subscriber)){
+                return;
+
+            }else{
+                sublist[channel].push(subscriber);
+            }
+        },
+        notify: function(channel){
+            if(sublist[channel])
+                for (const subscriber of sublist[channel]){
+                    subscriber();
+             }
+        }
+    }
+
+
+}
+
+
+const publisher = PubProto();
+
+const registerChannel = _.curry(publisher.subscribe);
+const registerChannel1 = registerChannel("1");
+
+const registerChannel2 = registerChannel("2");
+
+const sub1 =  ()=>{console.log("hihi")}
+const sub2 =  ()=>{console.log("hihi2")}
+
+
+registerChannel2(sub2);
+registerChannel1(sub1);
+
+// publisher.subscribe("1",sub1)
+// publisher.subscribe("1",sub1)
+// publisher.subscribe("1",sub2);
+
+
+publisher.notify("1");
+publisher.notify("2");
+// publisher.notify("2");
+
+
+
+
