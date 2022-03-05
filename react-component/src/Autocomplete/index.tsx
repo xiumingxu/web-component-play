63545d5234b5b3a1a7dec5c23e8260f6722dee4d
xx <xiuming.x@outlook.com>
2022-03-05 11:59:41 -0600

    改写奇怪的

--- react-component/src/Autocomplete/index.tsx
+++ react-component/src/Autocomplete/index.tsx
@@ -3,40 +3,63 @@ import React, {useRef, FC, useCallback, ChangeEvent, ChangeEventHandler, useStat
 //Test data
 interface TestType {
     value: string
+    id: number
+    other: string
 }
 
-const returnValue: OptionType[] =  Array(5).fill(0).map(( value, idx)=>({id:idx,  value: idx + 'hehe'}))
 
 interface ObjectWithValue {
     value: string;
 }
 
-type OptionType = ObjectWithValue & {}
+type OptionType<T={}> = ObjectWithValue & T
 
-interface AutocompleteProps<T extends OptionType>{
-    fetchSuggestions: (inputValue: string)=>Promise<T[]> | T[]
+interface AutocompleteProps{ //并没有对interface进行抽象
+    fetchSuggestions: (inputValue: string)=>Promise<OptionType[]> | OptionType[]
+    onSelect: (item: OptionType)=>void
 }
 
 
-const initValue : AutocompleteProps<TestType> = {
+const initValue : AutocompleteProps = {
     fetchSuggestions : (inputValue: string)=>{
-        console.log("currentinput", inputValue)
+        const returnValue: TestType[] =  Array(5).fill(0).map(( value, idx)=>({id:idx,  other: 'test', value: idx + inputValue}))
         return returnValue
+    },
+    onSelect: (item)=>{
+        console.log(item)
     }
 
 }
 //?咋用
-export const Autocomplete : FC<T>= (props:AutocompleteProps<T extends OptionType> = initValue)=>{
-    const {fetchSuggestions} = initValue
+export const Autocomplete: FC<AutocompleteProps>= (props
+)=>{
+    const {fetchSuggestions, onSelect} = initValue
     const inputValue = useRef<HTMLInputElement>(null)
-    const [options, setoptions] = useState<OptionType>()
+    const [options, setOptions] = useState<OptionType[]>([])
+    const [selectedOption, setSelectedOption] = useState<OptionType>()
+
     const handleChange = useCallback((_: ChangeEvent)=>{
-        fetchSuggestions((inputValue.current as HTMLInputElement).value)
-        // inputValue.current.value = event.target.value
+        console.log("triggered")
+        const results = fetchSuggestions((inputValue.current as HTMLInputElement).value)
+        if (results instanceof Promise){
+            results.then(options=> { setOptions(options)})
+        }else{
+            setOptions(results)
+        }
     },[])
+
+    //怎么写不用
+    const handleSelect = useCallback((idx)=>(event: React.MouseEvent<HTMLLIElement> )=>{
+        const curOption = options[idx]
+        setSelectedOption(curOption)
+        setOptions([])
+        onSelect(curOption)
+
+        inputValue.current!.value = curOption.value
+    },[options, inputValue])
 
     return <>
         <input ref={inputValue} onChange={handleChange}/>
-        <></>
+        {options.length !==0 && options.map((option, idx)=>(<li key={idx} onClick={handleSelect(idx)}>{option.value}</li>))}
     </>
 }
